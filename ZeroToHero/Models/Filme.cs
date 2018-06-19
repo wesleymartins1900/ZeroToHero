78df8dd1bf777f7433b2e6efd27f4174bc2adfaf
Wesley Martins <wesley.martins1900@gmail.com>
2018-06-19 19:30:36 -0300

    Commit Inicial

---
+++ ZeroToHero/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZeroToHero.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public DateTime AssistidoEm { get; set; }
+        public int Status { get; set;
+        }
+    }
+}
